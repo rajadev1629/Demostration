7a6cb752d3a86ebe1abc029238107c782fa1a300
Raja kumar <raja29cse@gmail.com>
2014-11-25 12:40:53 +0530

    Add a new file

---
+++ src/com/example/demostration/Second.java
@@ -0,0 +1,14 @@
+package com.example.demostration;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Second extends Activity{
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		//Add some more comment
+	}
+
+}
